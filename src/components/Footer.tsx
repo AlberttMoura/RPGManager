791ad5f8cf975c286ddad7f6f5e920b537e58713
AlberttMoura <alberrttmoura@gmail.com>
2022-03-11 15:14:47 -0300

    Footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,35 @@
 import { Container } from './index'
+import styled from 'styled-components'
+
+const FooterLinks = styled.p`
+	font-size: 14px;
+	font-weight: 400;
+	color: #ffffff;
+	margin: 10px 0px;
+`
 
 export const Footer = () => {
 	return (
-		<Container>
-			<h1>Hello World</h1>
+		<Container
+			style={{
+				backgroundColor: '#343434',
+				width: '100%',
+				margin: '20px 0px 0px 0px',
+			}}>
+			<Container
+				style={{
+					width: '100%',
+					flexDirection: 'row',
+					justifyContent: 'space-evenly',
+				}}>
+				<FooterLinks>About</FooterLinks>
+				<FooterLinks>Contact us</FooterLinks>
+				<FooterLinks>Help</FooterLinks>
+				<FooterLinks>Terms and rules</FooterLinks>
+			</Container>
+			<Container>
+				<FooterLinks>Warfog ® 2022 - All rights reserved</FooterLinks>
+			</Container>
 		</Container>
 	)
 }
