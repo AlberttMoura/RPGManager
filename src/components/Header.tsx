0c410bf3ac211a04480600fad5b5c2710f6d5cdf
AlberttMoura <alberrttmoura@gmail.com>
2022-03-11 10:14:06 -0300

    Initial structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+	return (
+		<div>
+			<h1>Oi</h1>
+		</div>
+	)
+}
---
+++ src/components/styles/Container.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	max-width: 100%;
+	padding: 0 auto;
+	margin: 0 auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	text-align: center;
+`
